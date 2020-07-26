3c764f3a3be2a9317651b831973b63894e31525f
saiful islam <saiful01636758237@gmail.com>
2020-07-26 09:38:18 +0600

    edu board2 setup

---
+++ Admin/index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en" class=" ">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Admin log in</title>
+    <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="stylesheet" href="css/app.v1.css" type="text/css" />
+    <!--[if lt IE 9]> <script src="js/ie/html5shiv.js"></script> <script src="js/ie/respond.min.js"></script> <script src="js/ie/excanvas.js"></script> <![endif]-->
+</head>
+
+<body class="">
+
+
+    <section id="content" class="m-t-lg wrapper-md animated fadeInUp">
+        <div class="container aside-xl"> <a class="navbar-brand block" href="index.html">Admin log in</a>
+            <hr>
+            
+            <section class="m-b-lg">
+
+
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method ="POST">
+                    <div class="list-group">
+                        <div class="list-group-item">
+                            <input name="email_uname" type="text" placeholder="Email / username" class="form-control no-border"> </div>
+                        <div class="list-group-item">
+                            <input name="pass" type="password" placeholder="Password" class="form-control no-border"> </div>
+                    </div>
+                    <button name="log" type="submit" class="btn btn-lg btn-primary btn-block">Log in</button>
+                    <div class="line line-dashed"></div>
+                </form>
+                    
+            </section>
+        </div>
+    </section>
+    <!-- footer -->
+    <!-- / footer -->
+    <!-- Bootstrap -->
+    <!-- App -->
+    <script src="js/app.v1.js"></script>
+    <script src="js/app.plugin.js"></script>
+</body>
+<!-- Mirrored from flatfull.com/themes/scale/signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jul 2019 15:15:07 GMT -->
+
+</html>
